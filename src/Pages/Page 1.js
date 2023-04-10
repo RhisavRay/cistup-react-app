8baaaed903a889fd442cfeca4aed4d6c43340025
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-04-10 08:49:25 +0530

    cosmetics

--- src/Pages/Page 1.js
+++ src/Pages/Page 1.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Page2 from "./Page 2"
+import './page.css'
 
 function Page1()
 {
@@ -35,54 +36,55 @@ function Page1()
     return(
         <div>
             {/* Question 1 */}
-            <h3>What is your most frequently used means of travel from your home to work location?</h3>
-            <input type="radio" name="trans" id="bus" value="bus" onChange={transChangeHandler} checked={transMode==="bus"}/>
+            <h3 className="Q">What is your most frequently used means of travel from your home to work location?</h3>
+            <input type="radio" name="trans" id="bus" value="bus" onChange={transChangeHandler} checked={transMode==="bus"} className="A"/>
             <label htmlFor="bus">Bus</label>
             <br/>
-            <input type="radio" name="trans" id="metro" value="metro" onChange={transChangeHandler} checked={transMode==="metro"}/>
+            <input type="radio" name="trans" id="metro" value="metro" onChange={transChangeHandler} checked={transMode==="metro"} className="A"/>
             <label htmlFor="metro">Metro</label>
             <br/>
-            <input type="radio" name="trans" id="own2Wheeler" value="own2Wheeler" onChange={transChangeHandler} checked={transMode==="own2Wheeler"}/>
+            <input type="radio" name="trans" id="own2Wheeler" value="own2Wheeler" onChange={transChangeHandler} checked={transMode==="own2Wheeler"} className="A"/>
             <label htmlFor="own2Wheeler">Own Two-wheeler</label>
             <br/>
-            <input type="radio" name="trans" id="ownCar" value="ownCar" onChange={transChangeHandler} checked={transMode==="ownCar"}/>
+            <input type="radio" name="trans" id="ownCar" value="ownCar" onChange={transChangeHandler} checked={transMode==="ownCar"} className="A"/>
             <label htmlFor="ownCar">Own Car</label>
             <br/>
-            <input type="radio" name="trans" id="walk/bicycle" value="walk/bicycle" onChange={transChangeHandler} checked={transMode==="walk/bicycle"}/>
+            <input type="radio" name="trans" id="walk/bicycle" value="walk/bicycle" onChange={transChangeHandler} checked={transMode==="walk/bicycle"} className="A"/>
             <label htmlFor="walk/bicycle">Walk / Bicycle</label>
             <br/>
-            <input type="radio" name="trans" id="auto" value="auto" onChange={transChangeHandler} checked={transMode==="auto"}/>
+            <input type="radio" name="trans" id="auto" value="auto" onChange={transChangeHandler} checked={transMode==="auto"} className="A"/>
             <label htmlFor="auto">Auto</label>
             <br/>
-            <input type="radio" name="trans" id="cab" value="cab" onChange={transChangeHandler} checked={transMode==="cab"}/>
+            <input type="radio" name="trans" id="cab" value="cab" onChange={transChangeHandler} checked={transMode==="cab"} className="A"/>
             <label htmlFor="cab">App based ride hailing cab services including Ola / Uber</label>
             
 
             <br/><br/>
 
             {/* Question 2 */}
-            <h3>What is the total distance between your home and workplace?</h3>
-            <input type="radio" name="dist" id="<5" value="<5" onChange={distChangeHandler} checked={dist==="<5"}/>
+            <h3 className="Q">What is the total distance between your home and workplace?</h3>
+            <input type="radio" name="dist" id="<5" value="<5" onChange={distChangeHandler} checked={dist==="<5"} className="A"/>
             <label htmlFor="<5">&lt;5km</label>
             <br/>
-            <input type="radio" name="dist" id="5-10" value="5-10" onChange={distChangeHandler} checked={dist==="5-10"}/>
+            <input type="radio" name="dist" id="5-10" value="5-10" onChange={distChangeHandler} checked={dist==="5-10"} className="A"/>
             <label htmlFor="5-10">5-10km</label>
             <br/>
-            <input type="radio" name="dist" id="10-15" value="10-15" onChange={distChangeHandler} checked={dist==="10-15"}/>
+            <input type="radio" name="dist" id="10-15" value="10-15" onChange={distChangeHandler} checked={dist==="10-15"} className="A"/>
             <label htmlFor="10-15">10-15km</label>
             <br/>
-            <input type="radio" name="dist" id="15-20" value="15-20" onChange={distChangeHandler} checked={dist==="15-20"}/>
+            <input type="radio" name="dist" id="15-20" value="15-20" onChange={distChangeHandler} checked={dist==="15-20"} className="A"/>
             <label htmlFor="15-20">15-20km</label>
             <br/>
-            <input type="radio" name="dist" id="20-25" value="20-25" onChange={distChangeHandler} checked={dist==="20-25"}/>
+            <input type="radio" name="dist" id="20-25" value="20-25" onChange={distChangeHandler} checked={dist==="20-25"} className="A"/>
             <label htmlFor="20-25">20-25km</label>
             <br/>
-            <input type="radio" name="dist" id=">25" value=">25" onChange={distChangeHandler} checked={dist===">25"}/>
+            <input type="radio" name="dist" id=">25" value=">25" onChange={distChangeHandler} checked={dist===">25"} className="A"/>
             <label htmlFor=">25">&gt;25km</label>
             
             <br/>
+            <br/>
 
-            <button onClick={nextHandler}>Next</button>
+            <button onClick={nextHandler} className="next">Next</button>
         </div>
     )
 }
